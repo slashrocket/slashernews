9a66a6b09da91f19c68b297f765c15b3e474b751
askl56 <amscott@openmailbox.org>
2015-05-24 06:55:24 +0100

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   root to: 'application#angular'
+
+  resources :posts, only: [:create, :index, :show] do
+    resources :comments, only: [:show, :create] do
+      member do
+        put '/upvote' => 'comments#upvote'
+      end
+
+      member do
+        put '/upvote' => 'posts#upvote'
+      end
+    end
+  end
 end
