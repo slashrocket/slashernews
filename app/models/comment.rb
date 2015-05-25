24028680731f18059c2d0644c16bd5f5ac60a3f9
Daniel Climent <stokp1@gmail.com>
2015-05-25 13:49:50 +0300

    remove as_json methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,4 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
-  
-  def as_json(options = {})
-    super(options.merge(include: [:user, comments: {include: :user}]))
-  end
 end
