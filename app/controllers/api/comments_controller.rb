99ba11efb3df5ff495bbb6c2302fa0f530e2302d
Daniel Climent <stokp1@gmail.com>
2015-05-25 12:51:16 +0300

    move controllers to api module

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  class CommentsController < ApplicationController
+    before_filter :authenticate_user!, only: [:create, :upvote]
+
+    def create
+      post = Post.find(params[:post_id])
+      comment = post.comments.create(comment_params)
+      respond_with post, comment
+    end
+
+    def upvote
+      post = Post.find(params[:post_id])
+      comment = post.comments.find(params[:id])
+      comment.increment!(:upvotes)
+
+      respond_with post, comment
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+  end
+end
---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  class PostsController < ApplicationController
+    before_filter :authenticate_user!, only: [:create, :upvote]
+
+    def index
+      respond_with Post.all
+    end
+
+    def create
+      respond_with Post.create(post_params)
+    end
+
+    def show
+      respond_with Post.find(params[:id])
+    end
+
+    def upvote
+      post = Post.find(params[:id])
+      post.increment!(:upvotes)
+
+      respond_with post
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:link, :title)
+    end
+  end
+end
