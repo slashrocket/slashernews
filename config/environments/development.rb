77c0318e5b77df7876344b5e69c1cb8048adc2ac
Daniel Climent <stokp1@gmail.com>
2015-06-02 19:03:24 +0300

    add configuration for Bullet gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,14 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.after_initialize do
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+  Bullet.rails_logger = true
+  Bullet.add_footer = true
+end
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
