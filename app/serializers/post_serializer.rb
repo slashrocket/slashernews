848774af0f79adda90dae77b94109d02c0372853
Daniel Climent <stokp1@gmail.com>
2015-06-03 17:52:22 +0300

    return a default value if the post doesn't have a thumbnail

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,4 +2,8 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :link, :upvotes, :thumbnail, :voters ,:created_at
   has_one :user
   has_many :comments
+
+  def thumbnail
+    object.thumbnail.present? ? object.thumbnail : '//img.imgur.com/NAhYNUt.jpg'
+  end
 end
