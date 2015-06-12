5bf6c68c3a1d4f461d99155b235ef81da3048a58
askl56 <amscott@openmailbox.org>
2015-06-12 10:30:57 +0100

    fixed ruby 1.9 hash syntax

--- spec/controllers/regression/api/comments_controller_spec.rb
+++ spec/controllers/regression/api/comments_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Api::CommentsController, regressor: true do
   # === Routes (REST) ===
-  it { should route(:post, '/api/posts/1/comments').to('api/comments#create', {:post_id=>"1", :format=>:json}) } 
-	it { should route(:put, '/api/posts/1/comments/1/upvote').to('api/comments#upvote', {:post_id=>"1", :comment_id=>"1", :format=>:json}) } 
+  it { should route(:post, '/api/posts/1/comments').to('api/comments#create', {post_id:"1", format::json}) } 
+	it { should route(:put, '/api/posts/1/comments/1/upvote').to('api/comments#upvote', {post_id:"1", comment_id:"1", format::json}) } 
   # === Callbacks (Before) ===
   it { should use_before_filter(:verify_authenticity_token) }
 	it { should use_before_filter(:configure_permitted_parameters) }
--- spec/controllers/regression/api/posts_controller_spec.rb
+++ spec/controllers/regression/api/posts_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Api::PostsController, regressor: true do
   # === Routes (REST) ===
-  it { should route(:get, '/api/posts').to('api/posts#index', {:format=>:json}) }
-	it { should route(:get, '/api/posts/1').to('api/posts#show', {:id=>"1", :format=>:json}) }
-	it { should route(:post, '/api/posts').to('api/posts#create', {:format=>:json}) } 
-	it { should route(:put, '/api/posts/1/upvote').to('api/posts#upvote', {:post_id=>"1", :format=>:json}) } 
+  it { should route(:get, '/api/posts').to('api/posts#index', {format::json}) }
+	it { should route(:get, '/api/posts/1').to('api/posts#show', {id:"1", format::json}) }
+	it { should route(:post, '/api/posts').to('api/posts#create', {format::json}) } 
+	it { should route(:put, '/api/posts/1/upvote').to('api/posts#upvote', {post_id:"1", format::json}) } 
   # === Callbacks (Before) ===
   it { should use_before_filter(:verify_authenticity_token) }
 	it { should use_before_filter(:configure_permitted_parameters) }
